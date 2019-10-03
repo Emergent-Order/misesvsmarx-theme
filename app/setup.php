0c54192ab5e99d636cb51dbe6d8e08bd2ff88ab3
Ellen Bartling <ellen@emergentorder.com>
2019-10-03 16:40:11 -0500

    Remove finicky svg compiler

--- app/setup.php
+++ app/setup.php
@@ -137,16 +137,16 @@ add_action('after_setup_theme', function () {
     });
 
     // Create SVG directive
-    sage('blade')->compiler()->directive('svg', function ($arguments) {
-      list($path, $class) = array_pad(explode(',', trim($arguments, "() ")), 2, '');
-      $path = trim($path, "' ");
-      $class = trim($class, "' ");
-
-      $svg = new \DOMDocument();
-      $svg->load(asset_path($path));
-      $svg->documentElement->setAttribute("class", $class);
-      $output = $svg->saveXML($svg->documentElement);
-
-      return $output;
-    });
+    // sage('blade')->compiler()->directive('svg', function ($arguments) {
+    //   list($path, $class) = array_pad(explode(',', trim($arguments, "() ")), 2, '');
+    //   $path = trim($path, "' ");
+    //   $class = trim($class, "' ");
+    //
+    //   $svg = new \DOMDocument();
+    //   $svg->load(asset_path($path));
+    //   $svg->documentElement->setAttribute("class", $class);
+    //   $output = $svg->saveXML($svg->documentElement);
+    //
+    //   return $output;
+    // });
 });
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <header class="banner bg-gold-light">
   <div class="container mx-auto py-4">
     <a class="brand hidden lg:flex" href="{{ home_url('/') }}">
-      @svg('../resources/assets/images/logo-full.svg', 'logo')
+      <img src="@asset('../resources/assets/images/logo-full.svg')" />
     </a>
-    <a class="brand flex lg:hidden" href="{{ home_url('/') }}">
-      @svg('../resources/assets/images/logo.svg', 'logo')
-      @svg('../resources/assets/images/logo-sub-oneline.svg', 'logo')
+    <a class="brand flex justify-start lg:hidden" href="{{ home_url('/') }}">
+      <img src="@asset('../resources/assets/images/logo.svg')" />
+      <img src="@asset('../resources/assets/images/logo-sub-oneline.svg')" />
     </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
