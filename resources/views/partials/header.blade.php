16bcfd2b2d489ac9133d4f1da62ee7c8e14d8577
Ellen Bartling <ellen@emergentorder.com>
2019-10-07 15:04:48 -0500

    Add alt for top image tag

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="banner bg-gold-light">
   <div class="container flex justify-center mx-auto py-4">
     <a class="brand" href="{{ home_url('/') }}">
-      <img src="@asset('/images/logo-title.svg')" />
+      <img alt="The March of History" src="@asset('/images/logo-title.svg')" />
     </a>
   </div>
 </header>
