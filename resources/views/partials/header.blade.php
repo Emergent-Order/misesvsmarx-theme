91ecd3f662cb34d858fda4ba9c3df9d27cb4bf00
Ellen Bartling <ellen@emergentorder.com>
2019-10-07 15:03:44 -0500

    Swap out header for new content

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,7 @@
 <header class="banner bg-gold-light">
-  <div class="container mx-auto py-4">
-    <a class="brand hidden lg:flex" href="{{ home_url('/') }}">
-      <img src="@asset('/images/logo-full.svg')" />
+  <div class="container flex justify-center mx-auto py-4">
+    <a class="brand" href="{{ home_url('/') }}">
+      <img src="@asset('/images/logo-title.svg')" />
     </a>
-    <a class="brand flex justify-start lg:hidden" href="{{ home_url('/') }}">
-      <img src="@asset('/images/logo.svg')" />
-      <img src="@asset('/images/logo-sub-oneline.svg')" />
-    </a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
   </div>
 </header>
